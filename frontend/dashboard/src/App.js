722475cb1dda809c7054c88471edd2555d60ba98
aliseidu8855 <aliseidu8855@gmail.com>
2024-12-09 23:17:20 +0000

    Add Login component to frontend dashboard

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import Login from "./components/Login";
 
 const App = () => {
     return (
         <div>
             <Header />
             <MainContent />
+            <Login />
         </div>
     );
 };
